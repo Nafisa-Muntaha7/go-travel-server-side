ceb2e20d051a10a1e62117c3338b8499b28e2693
Nafisa-Muntaha7 <farjanapopi8@gmail.com>
2021-10-30 12:00:55 +0600

    database connected

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+
 const app = express();
-const port = 7000;
+const port = process.env.PORT || 7000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mrxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('travel_Agent');
+        const tourCollection = database.collection('tours');
+
+        app.get('/tours', async (req, res) => {
+            const cursor = tourCollection.find({});
+            const tours = await cursor.toArray();
+            res.send(tours);
+        })
+
+    }
+    finally {
+        //await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Server Running in localhost')
@@ -9,3 +40,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log('Server Running.....')
 })
+
+//go-travel
+//gvMkDajwdMW48hxh
