1a69040fd04d9bd9ba569f9b077b4605c028623f
Nafisa-Muntaha7 <farjanapopi8@gmail.com>
2021-10-31 16:45:52 +0600

    heroku issue fixed

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5mrxq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//console.log(uri);
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -27,25 +28,22 @@ async function run() {
             res.send(tours);
         })
 
-
         app.get('/bookings', async (req, res) => {
             const cursor = bookingCollection.find({});
             const bookings = await cursor.toArray();
             res.send(bookings);
         })
 
-
         app.delete('/tours/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await tourCollection.deleteOne(query);
-            console.log('Count', result)
-            res.json(result)
+            res.json(result);
         })
 
     }
     finally {
-        //await client.close();
+        await client.close();
     }
 }
 
